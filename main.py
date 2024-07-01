9acc7e3ebdd39f7e45a1aa6eec15974e2b85e799
Alex <guardamar81@gmail.com>
2024-07-01 20:59:22 +0400

    animal.csv

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ import pandas as pd
 # df = pd.DataFrame(data)
 # print(df)
 
-df = pd.read_csv('World-happiness-report-2024.csv')
+#df = pd.read_csv('World-happiness-report-2024.csv')
 #print(df.head())
 #print(df.tail())
 #print(df.info())
@@ -21,4 +21,13 @@ df = pd.read_csv('World-happiness-report-2024.csv')
 #print(df[['Country name', 'Regional indicator']])
 #print(df.loc[56])
 #print(df.loc[56, 'Healthy life expectancy'])
-print(df[df['Healthy life expectancy'] > 0.7])
+#print(df[df['Healthy life expectancy'] > 0.7])
+
+df = pd.read_csv('hh.csv')
+print(df)
+# df['Test'] = [new for new in range(29)]
+# print(df)
+# df.drop('Test', axis=1, inplace=True)
+# print(df)
+df.drop(28, axis=0, inplace=True)
+print(df)
